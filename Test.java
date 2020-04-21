d68d4e26d6471627ea2664cbd929e24046b8bc76
kwj <koj135011@gmail.com>
2020-04-21 19:41:04 +0900

    update second

--- Test.java
+++ Test.java
@@ -4,8 +4,7 @@ public class Test {
 	System.out.println("출력하고 싶은 단 입력 :");
 	int num = sc.nextInt();
 	if(num == 0) {
-		for(int i = 2; i <=9; i++) {
-			for(int j =1 ; j<=9 ; j++) {
-				System.out.print(i + " x " + j + " = " + i*j + " ");
-			}
-}
+		for(int i = 2; i <=9; i++)safasfsaf++) {
+				System.out.print(i sd+ " x " + j + " = " + i*j + " ");
+			}dfsdfa
+}asdfasfsafsad
